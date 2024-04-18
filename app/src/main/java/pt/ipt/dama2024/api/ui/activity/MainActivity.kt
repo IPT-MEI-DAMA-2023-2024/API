69865038acafc1031525d15a1cdd1025d745fd92
jcnpereira <casimiro@ipt.pt>
2024-04-18 21:49:37 +0100

    main code of our app

---
+++ app/src/main/java/pt/ipt/dama2024/api/ui/activity/MainActivity.kt
@@ -0,0 +1,71 @@
+package pt.ipt.dama2024.api.ui.activity
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import pt.ipt.dama2024.api.R
+import pt.ipt.dama2024.api.model.Note
+import pt.ipt.dama2024.api.retrofit.RetrofitInitializer
+import pt.ipt.dama2024.api.ui.adpter.NoteListAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // start the read of API data, and use it
+        listNotes()
+    }
+
+    private fun listNotes() {
+        // here we are actually reading the API data
+        val call = RetrofitInitializer().noteService().getNotes()
+        // do something with the received data
+        processList(call)
+    }
+
+    /**
+     * Process list that was provided by API
+     *
+     * @param call list of notes
+     */
+    private fun processList(call: Call<List<Note>>) {
+        call.enqueue(object : Callback<List<Note>?> {
+            override fun onResponse(call: Call<List<Note>?>?,
+                                    response: Response<List<Note>?>?) {
+                response?.body()?.let {
+                    val notes: List<Note> = it
+                     configureList(notes)
+                }
+            }
+            override fun onFailure(call: Call<List<Note>?>?, t: Throwable?) {
+                t?.printStackTrace()
+                t?.message?.let { Log.e("onFailure error", it) }
+            }
+        })
+    }
+
+    private fun configureList(notes: List<Note>) {
+        val recyclerView: RecyclerView = findViewById(R.id.nodeList)
+        recyclerView.adapter = NoteListAdapter(notes, this)
+        // how the card will be organized
+        val layoutManager = StaggeredGridLayoutManager(
+            2, StaggeredGridLayoutManager.VERTICAL
+        )
+        recyclerView.layoutManager = layoutManager
+    }
+}
