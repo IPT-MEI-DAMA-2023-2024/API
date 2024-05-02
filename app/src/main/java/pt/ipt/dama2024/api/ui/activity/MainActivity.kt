7ed0f7501113b02ccbc2e9694afa704265c24021
jcnpereira <casimiro@ipt.pt>
2024-05-02 19:59:02 +0100

    send the new note to API

--- app/src/main/java/pt/ipt/dama2024/api/ui/activity/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama2024/api/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package pt.ipt.dama2024.api.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,8 +17,14 @@ import pt.ipt.dama2024.api.ui.adpter.NoteListAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.GregorianCalendar
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    // 'pointer' to the button
+    private lateinit var bt: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,6 +37,59 @@ class MainActivity : AppCompatActivity() {
 
         // start the read of API data, and use it
         listNotes()
+
+        // catch the 'click' event
+        bt=findViewById(R.id.btNewNote)
+        bt.setOnClickListener {
+            addNewNote()
+        }
+    }
+
+    /**
+     * create a random note     *
+     */
+    private fun addNewNote() {
+        val i = Random(GregorianCalendar.getInstance().timeInMillis).nextInt(100)
+        val note = Note("Note " + i, "Description $i")
+
+        addNote(note) {
+            Toast.makeText(this, "Added " + it?.description, Toast.LENGTH_LONG).show()
+            listNotes()
+
+        }
+    }
+
+    /**
+     * function that really add the note to API
+     *
+     * @param note
+     * @param function
+     * @receiver
+     */
+    private fun addNote(note: Note, onResult: (Note?) -> Unit) {
+        val call = RetrofitInitializer().noteService().addNote(note)
+        call.enqueue(
+            object : Callback<Note> {
+                /**
+                 * Invoked for a received HTTP response.
+                 * Note: An HTTP response may still indicate an application-level failure such as a 404 or 500.
+                 * Call [Response.isSuccessful] to determine if the response indicates success.
+                 */
+                override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                    val addedNote = response.body()
+                    onResult(addedNote)
+                }
+
+                /**
+                 * Invoked when a network exception occurred talking to the server or when an unexpected exception
+                 * occurred creating the request or processing the response.
+                 */
+                override fun onFailure(call: Call<Note>, t: Throwable) {
+                    t.printStackTrace()
+                    onResult(null)
+                }
+            }
+        )
     }
 
     private fun listNotes() {
